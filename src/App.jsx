87c6f3bc643c98ee443a190f5037eeaa78ac2c42
damoon darvishi <damoon.darvishi@gmail.com>
2024-07-08 10:40:14 +0200

    add spinner

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,30 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { Suspense } from "react";
+import SpinnerFullPage from "./components/SpinnerFullPage";
 
 function App() {
   return (
     <CitiesProvider>
       <BrowserRouter>
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="login" element={<Login />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<Navigate to="cities" replace />} />
-            <Route path="cities" element={<CityList />} />
-            <Route path="cities/:id" element={<City />} />
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<SpinnerFullPage />}>
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="login" element={<Login />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="app" element={<AppLayout />}>
+              <Route index element={<Navigate to="cities" replace />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </CitiesProvider>
   );
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -39,6 +39,7 @@ function Map() {
     },
     [geoLocationPosition]
   );
+
   return (
     <div className={styles.mapContainer}>
       {!geoLocationPosition && (
